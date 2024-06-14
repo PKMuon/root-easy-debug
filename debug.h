404a713c3e11b42ccdaa3b294b690e2baeb7b8fb
lyazj <seeson@pku.edu.cn>
2024-06-15 07:01:37 +0800

    Enable signal safety; support external debugger

--- debug.h
+++ debug.h
@@ -3,8 +3,11 @@
 // Written by <lyazj@github.com>.
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/prctl.h>
+#include <errno.h>
 #include <err.h>
 
 #if __has_builtin(__builtin_debugtrap)  // This is expected to work for Clang/LLVM.
@@ -14,21 +17,92 @@
 #define breakpoint()  ({ __asm("int3"); })  // [XXX] This assumes x86_64. Change it on your own need.
 #endif  /* __builtin_debugtrap */
 
-static void begin_debug()  // SIGCONT used exclusively.
+static int begin_debug(bool external = false)
 {
-  struct SignalHandler {
-    static void handler(int) { /* do nothing */ }
+  struct Inner {
+    static int ptrace_scope() {
+      FILE *file = fopen("/proc/sys/kernel/yama/ptrace_scope", "r");
+      if(file == NULL) return -1;
+
+      char buf[64] = {0};
+      fgets(buf, sizeof buf, file);
+      fclose(file);
+
+      char *end;
+      int value = strtol(buf, &end, 10);
+      if(end == buf) return -1;
+      return value;
+    }
+
+    static int tracer_pid() {
+      pid_t pid = -1;
+
+      FILE *file = fopen("/proc/self/status", "r");
+      if(file == NULL) return pid;
+
+      char buf[1024];
+      while(fgets(buf, sizeof buf, file)) {
+        if(strncmp(buf, "TracerPid:", 10) == 0) {
+          char *end;
+          long value = strtol(buf + 10, &end, 10);
+          if(end != buf + 10) pid = value;
+          break;
+        }
+      }
+
+      fclose(file);
+      return pid;
+    }
+
+    static void sigcont_handler(int) { /* do nothing */ }
+
+    static void wait_for_tracer() {
+      while(tracer_pid() <= 0) {  // False signal eliminated.
+        sighandler_t old_sigcont_handler = signal(SIGCONT, sigcont_handler);
+        pause();
+        signal(SIGCONT, old_sigcont_handler);
+      }
+    }
+
+    static void default_sigtrap_handler(int) {
+      char msg[] = "FATAL: SIGTRAP received without a proper debugger attached\n";
+      write(STDERR_FILENO, msg, sizeof msg - 1);
+      _exit(1);
+    }
   };
 
+  if(external) {
+    if(Inner::ptrace_scope() == 1) {
+      // Enable non-direct-ancestor ptrace for external debuggers.
+      prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY, 0, 0, 0);
+    }
+
+    printf("\nNow run the following command in another terminal:\n");
+    printf("\n");
+    //printf("    cgdb");
+    printf("    gdb");
+    //printf(" --quiet");
+    printf(" --eval-command '%s'", "set pagination off");  // Avoid being blocked by screen height.
+    printf(" --eval-command '%s'", "signal SIGCONT");
+    printf(" --eval-command '%s'", "set pagination on");
+    printf(" --pid %d", getpid());
+    printf("\n");
+    Inner::wait_for_tracer();
+    return 0;
+  }
+
   pid_t pid = fork();
-  if(pid < 0) err(EXIT_FAILURE, "fork");
+  if(pid < 0) {
+    int e = errno;
+    warn("fork");
+    signal(SIGTRAP, Inner::default_sigtrap_handler);
+    errno = e;
+    return -1;
+  }
 
   if(pid == 0) {  // child
-    // [XXX] Block other signals here if they can occur before SIGCONT.
-    sighandler_t old_sigcont_handler = signal(SIGCONT, SignalHandler::handler);
-    pause();  // Wait for a debugger to be ready.
-    signal(SIGCONT, old_sigcont_handler);
-    return;
+    Inner::wait_for_tracer();
+    return 0;
   }
 
   // Run GDB to attach process pid.
@@ -40,5 +114,12 @@ static void begin_debug()  // SIGCONT used exclusively.
       "--eval-command", "signal SIGCONT",
       "--eval-command", "set pagination on",
       "--pid", pid_s, NULL);
-  err(EXIT_FAILURE, "execlp: gdb");
+
+  // Handle execlp failure.
+  warn("execlp");
+  int e = errno;
+  kill(pid, SIGKILL);
+  signal(SIGTRAP, Inner::default_sigtrap_handler);
+  errno = e;
+  return -1;
 }
